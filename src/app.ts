ebdd3907a30709fa972a470671c82af63d1e1a7d
tanimul-islam <tanimulislamdihan@gmail.com>
2026-08-04 13:15:59 -0400

    (init): Package Initialize

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "RentNest API is running",
+  });
+});
+
+export default app;
